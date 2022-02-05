ec649bd93b03193dcd85fa88ffbcf45b7eb68fb0
Peter Strömberg <pez@pezius.com>
2022-02-05 23:19:45 +0100

    Update run.sh to output better

--- experiments/java-bitset-perf-flicker/run.sh
+++ experiments/java-bitset-perf-flicker/run.sh
@@ -5,8 +5,7 @@ javac PrimeSieve.java
 RUNS=$(if [ -z "$1" ]; then echo 10; else echo "$1"; fi)
 for ((i = 0; i < RUNS; i++))
 do
-  echo "=== BEGIN RUN $i ==="
+  echo -n "$i - "
   java PrimeSieve
   #java -XX:+PrintCompilation PrimeSieve
-  echo "=== END RUN $i ==="
 done
